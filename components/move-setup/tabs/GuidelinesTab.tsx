7b208ca9da61081dcc73326fb091aed2e9c231ad
David  Anusontarangkul <anusontarangkul.d@gmail.com>
2025-10-18 00:49:34 -0700

    refactor

--- components/move-setup/tabs/GuidelinesTab.tsx
+++ components/move-setup/tabs/GuidelinesTab.tsx
@@ -4,12 +4,16 @@ import VerticalSectionGroup from "@/components/shared/VerticalSectionGroup";
 import ArrivalWindowSection from "../sections/ArrivalWindowSection";
 import PolicySection from "../sections/PolicySection";
 import { useSlugContext } from "@/contexts/SlugContext";
-import { useCompanyArrivalAndPolicies } from "@/hooks/arrivalWindow/useCompanyArrivalAndPolicies";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
 
 const GuidelinesTab = () => {
   const { companyId } = useSlugContext();
-  const result = useCompanyArrivalAndPolicies(companyId);
-
+  const result = useQuery<
+    typeof api.arrivalWindow.getCompanyArrivalAndPolicies
+  >(api.arrivalWindow.getCompanyArrivalAndPolicies, {
+    companyId,
+  });
   let content: React.ReactNode;
 
   switch (result) {
