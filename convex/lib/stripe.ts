bf93ab269fedd8484d6be53d645d14bc683f5ee1
David  Anusontarangkul <anusontarangkul.d@gmail.com>
2026-01-21 19:15:47 +0700

    update stripe

--- convex/lib/stripe.ts
+++ convex/lib/stripe.ts
@@ -1,9 +1,8 @@
-import { STRIPE_API_VERSION } from "@/types/const";
-import Stripe from "stripe";
 import { serverEnv } from "../backendUtils/serverEnv";
+import { Stripe } from "stripe";
 
 const { STRIPE_KEY } = serverEnv();
 
 export const stripe = new Stripe(STRIPE_KEY, {
-  apiVersion: STRIPE_API_VERSION,
+  apiVersion: "2025-02-24.acacia",
 });
